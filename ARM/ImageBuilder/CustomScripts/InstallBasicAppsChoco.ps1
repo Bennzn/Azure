7969815636e3b12e7e0a7e806f3b6eb4e2e6d0bc
Benjamin Graus <benjamin.graus@acs.it>
2020-05-19 14:51:54 +0200

    Choco Scripts for ImageBuilder

---
+++ ARM/ImageBuilder/CustomScripts/InstallBasicAppsChoco.ps1
@@ -0,0 +1,11 @@
+#Install AdobeReader with Choco
+choco install adobereader -y
+
+#Install Flash Player
+choco install flashplayerplugin -y
+
+#Install Chrome
+choco install googlechrome -y
+
+#Install 7-Zip
+choco install 7zip.install -y
