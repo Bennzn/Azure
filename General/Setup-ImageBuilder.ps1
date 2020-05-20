c95b3b855dc2fdaf9b213d50fe9c00e8cd92184c
Benjamin Graus <benjamin.graus@acs.it>
2020-05-20 10:18:48 +0200

    changed VM Size

--- General/Setup-ImageBuilder.ps1
+++ General/Setup-ImageBuilder.ps1
@@ -69,7 +69,7 @@ New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imag
 ##################################################################################
 
 $sigGalleryName= "imagebuilder_sig"
-$imageDefName ="AB-CustomW2k19"
+$imageDefName ="IB-CustomW2k19"
 
 # additional replication region
 $replRegion2="westeurop"
@@ -79,4 +79,60 @@ New-AzGallery -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGrou
 
 # create gallery definition
 New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType Windows -Publisher 'ACSResearch' -Offer 'Windows' -Sku 'Win2019'
+
+#######################################################################
+
+$templateUrl="https://raw.githubusercontent.com/Bennzn/Azure/master/ARM/ImageBuilder/TestDeploy/armIBTestDeploy.json"
+$templateFilePath = "armIBTestDeploy.json"
+
+Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
+
+((Get-Content -path $templateFilePath -Raw) -replace '<imageDefName>',$imageDefName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region2>',$replRegion2) | Set-Content -Path $templateFilePath
+
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2019-05-01-preview" -imageTemplateName $imageTemplateName -svclocation $location
+
+############################################################
+
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview" -Action Run -Force
+
+#########################################################
+
+### Step 1: Update context
+$currentAzureContext = Get-AzContext
+
+### Step 2: Get instance profile
+$azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
+    
+Write-Verbose ("Tenant: {0}" -f  $currentAzureContext.Subscription.Name)
+ 
+### Step 4: Get token  
+$token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+$accessToken=$token.AccessToken
+
+#############################################################################
+
+
+$managementEp = $currentAzureContext.Environment.ResourceManagerUrl
+
+
+$urlBuildStatus = [System.String]::Format("{0}subscriptions/{1}/resourceGroups/$imageResourceGroup/providers/Microsoft.VirtualMachineImages/imageTemplates/{2}?api-version=2019-05-01-preview", $managementEp, $currentAzureContext.Subscription.Id,$imageTemplateName)
+
+$buildStatusResult = Invoke-WebRequest -Method GET  -Uri $urlBuildStatus -UseBasicParsing -Headers  @{"Authorization"= ("Bearer " + $accessToken)} -ContentType application/json 
+$buildJsonStatus =$buildStatusResult.Content
+$buildJsonStatus
+
+
+########################################################
+
 
