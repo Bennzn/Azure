42ba743cdf2b023f240d81cd3297363856420453
Benjamin Graus <Benjamin.Graus@acs.it>
2020-05-28 16:17:29 +0200

    New IB Win10 Build

--- ARM/ImageBuilder/CustomScripts/InstallBasicAppsChoco.ps1
+++ ARM/ImageBuilder/CustomScripts/InstallBasicAppsChoco.ps1
@@ -9,3 +9,15 @@ choco install googlechrome -y
 
 #Install 7-Zip
 choco install 7zip.install -y
+
+#Install Google Chrome
+choco install googlechrome -y
+
+#Install VLC
+choco install vlc -y 
+
+#Install VCRedist 2015-2019
+choco install vcredist140 -y 
+
+#Install LibreOffice
+choco install libreoffice-fresh -y 
