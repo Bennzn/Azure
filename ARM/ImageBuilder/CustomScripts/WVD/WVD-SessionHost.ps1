824112140487cd590b9a35d97d929f902aefa65d
Benjamin Graus <Benjamin.Graus@acs.it>
2020-10-30 10:13:21 +0100

    New WVD-SessionHost Script

---
+++ ARM/ImageBuilder/CustomScripts/WVD/WVD-SessionHost.ps1
@@ -0,0 +1,273 @@
+
+<#Author       : Ben 
+# Source: Dean Cefola https://github.com/DeanCefola/Azure-WVD/blob/master/PowerShell/New-WVDSessionHost.ps1
+# Creation Date: 30.10.2020
+# Usage        : Windows Virtual Desktop Scripted Install
+
+#>
+
+##############################
+#    WVD Script Parameters   #
+##############################
+Param (        
+    [Parameter(Mandatory=$true)]
+        [string]$ProfilePath,
+    [Parameter(Mandatory=$false)]
+        [string]$Optimize = $true           
+)
+
+######################
+#    WVD Variables   #
+######################
+
+$LocalWVDpath            = "c:\temp\wvd\"
+$FSLogixURI              = 'https://aka.ms/fslogix_download'
+$FSInstaller             = 'FSLogixAppsSetup.zip'
+
+####################################
+#    Test/Create Temp Directory    #
+####################################
+if((Test-Path c:\temp) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating temp directory"
+    New-Item -Path c:\temp -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "temp directory already exists"
+}
+if((Test-Path $LocalWVDpath) -eq $false) {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Create C:\temp\WVD Directory"
+    Write-Host `
+        -ForegroundColor Cyan `
+        -BackgroundColor Black `
+        "creating c:\temp\wvd directory"
+    New-Item -Path $LocalWVDpath -ItemType Directory
+}
+else {
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "C:\temp\WVD Already Exists"
+    Write-Host `
+        -ForegroundColor Yellow `
+        -BackgroundColor Black `
+        "c:\temp\wvd directory already exists"
+}
+New-Item -Path c:\ -Name New-WVDSessionHost.log -ItemType File
+Add-Content `
+-LiteralPath C:\New-WVDSessionHost.log `
+"
+ProfilePath       = $ProfilePath
+Optimize          = $Optimize
+"
+
+
+#################################
+#    Download WVD Componants    #
+#################################
+
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Downloading FSLogix"
+    Invoke-WebRequest -Uri $FSLogixURI -OutFile "$LocalWVDpath$FSInstaller"
+
+
+##############################
+#    Prep for WVD Install    #
+##############################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Unzip FSLogix"
+Expand-Archive `
+    -LiteralPath "C:\temp\wvd\$FSInstaller" `
+    -DestinationPath "$LocalWVDpath\FSLogix" `
+    -Force `
+    -Verbose
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+cd $LocalWVDpath 
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "UnZip FXLogix Complete"
+
+Start-Sleep -Seconds 10
+
+#########################
+#    FSLogix Install    #
+#########################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing FSLogix"
+$fslogix_deploy_status = Start-Process `
+    -FilePath "$LocalWVDpath\FSLogix\x64\Release\FSLogixAppsSetup.exe" `
+    -ArgumentList "/install /quiet" `
+    -Wait `
+    -Passthru
+
+    Start-Sleep -Seconds 10
+
+#######################################
+#    FSLogix User Profile Settings    #
+#######################################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Configure FSLogix Profile Settings"
+Push-Location 
+Set-Location HKLM:\SOFTWARE\
+New-Item `
+    -Path HKLM:\SOFTWARE\FSLogix `
+    -Name Profiles `
+    -Value "" `
+    -Force
+New-Item `
+    -Path HKLM:\Software\FSLogix\Profiles\ `
+    -Name Apps `
+    -Force
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "Enabled" `
+    -Type "Dword" `
+    -Value "1"
+New-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "VHDLocations" `
+    -Value $ProfilePath `
+    -PropertyType MultiString `
+    -Force
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SizeInMBs" `
+    -Type "Dword" `
+    -Value "30000"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "IsDynamic" `
+    -Type "Dword" `
+    -Value "1"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "VolumeType" `
+    -Type String `
+    -Value "vhdx"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "FlipFlopProfileDirectoryName" `
+    -Type "Dword" `
+    -Value "1" 
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SIDDirNamePattern" `
+    -Type String `
+    -Value "%username%%sid%"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name "SIDDirNameMatch" `
+    -Type String `
+    -Value "%username%%sid%"
+Set-ItemProperty `
+    -Path HKLM:\Software\FSLogix\Profiles `
+    -Name DeleteLocalProfileWhenVHDShouldApply `
+    -Type DWord `
+    -Value 1
+Pop-Location
+
+
+#######################################################
+############### Installs Chocolatey ###################
+#######################################################
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Start-Sleep -Seconds 10
+
+######### Install Basic Apps ########################
+
+#Install AdobeReader with Choco
+choco install adobereader -y
+
+#Install Flash Player
+choco install flashplayerplugin -y
+
+#Install Chrome
+choco install googlechrome -y
+
+#Install 7-Zip
+choco install 7zip.install -y
+
+#Install VLC
+choco install vlc -y 
+
+#Install VCRedist 2015-2019
+choco install vcredist140 -y 
+
+#Install LibreOffice
+choco install libreoffice-fresh -y 
+
+
+
+#######################################################
+############### Installs Teams Optimized ##############
+# Used from https://github.com/RoelDU/WVDImaging/blob/master/Win10ms_O365.ps1
+#######################################################
+
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install C++ Redist for RTCSvc (Teams Optimized) ***'
+Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vc_redist.x64.exe' -OutFile 'c:\temp\vc_redist.x64.exe'
+Invoke-Expression -Command 'C:\temp\vc_redist.x64.exe /install /quiet /norestart'
+Start-Sleep -Seconds 10
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install RTCWebsocket to optimize Teams for WVD ***'
+New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -Force | Out-Null
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -Name 'IsWVDEnvironment' -Value '1' -PropertyType DWORD -Force | Out-Null
+Invoke-WebRequest -Uri 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4vkL6' -OutFile 'c:\temp\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi' 
+Invoke-Expression -Command 'msiexec /i c:\temp\MsRdcWebRTCSvc_HostSetup_0.11.0_x64.msi /quiet /l*v C:\temp\MsRdcWebRTCSvc_HostSetup.log ALLUSER=1'
+Start-Sleep -Seconds 15
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install Teams in Machine mode ***'
+Invoke-WebRequest -Uri 'https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.21759/Teams_windows_x64.msi' -OutFile 'c:\temp\Teams.msi'
+Invoke-Expression -Command 'msiexec /i C:\temp\Teams.msi /quiet /l*v C:\temp\teamsinstall.log ALLUSER=1 ALLUSERS=1'
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG TEAMS *** Configure Teams to start at sign in for all users. ***'
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run -Name Teams -PropertyType Binary -Value ([byte[]](0x01,0x00,0x00,0x00,0x1a,0x19,0xc3,0xb9,0x62,0x69,0xd5,0x01)) -Force
+Start-Sleep -Seconds 30
+
+
+##############################################
+#    WVD Optimizer (Virtual Desktop Team)    #
+##############################################
+If ($Optimize -eq $true) {  
+    Write-Output "Optimizer selected"  
+    ################################
+    #    Download WVD Optimizer    #
+    ################################
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize Selected"
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Creating C:\Optimize folder"
+    New-Item -Path C:\ -Name Optimize -ItemType Directory -ErrorAction SilentlyContinue
+    $LocalOptimizePath = "C:\Optimize\"
+    $WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+    $WVDOptimizeInstaller = "Windows_10_VDI_Optimize-master.zip"
+    Invoke-WebRequest `
+        -Uri $WVDOptimizeURL `
+        -OutFile "$LocalOptimizePath$WVDOptimizeInstaller"
+
+
+    ###############################
+    #    Prep for WVD Optimize    #
+    ###############################
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize downloaded and extracted"
+    Expand-Archive `
+        -LiteralPath "C:\Optimize\Windows_10_VDI_Optimize-master.zip" `
+        -DestinationPath "$LocalOptimizePath" `
+        -Force `
+        -Verbose
+    Set-Location -Path C:\Optimize\Virtual-Desktop-Optimization-Tool-master
+
+
+    #################################
+    #    Run WVD Optimize Script    #
+    #################################
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Begining Optimize"
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+    .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimization Complete"
+}
+else {
+    Write-Output "Optimize not selected"
+    Add-Content -LiteralPath C:\New-WVDSessionHost.log "Optimize NOT selected"    
+}
+
+
+##########################
+#    Restart Computer    #
+##########################
+Add-Content -LiteralPath C:\New-WVDSessionHost.log "Process Complete - REBOOT"
